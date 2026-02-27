79753fee4ebb4fd9a2aec7b8a7435d2f16da37a4
Shaik Aaron <shaikaaronbbhs@gmail.com>
2026-02-27 22:01:20 +0530

    feat: better auth state + refresh handling

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -1,9 +1,22 @@
 import axios from 'axios'
 
-export default axios.create({
+const api = axios.create({
     baseURL: 'https://heartos-production.up.railway.app/',
     headers: {
         'Content-Type': 'application/json'
     },
     withCredentials: true
 })
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            document.cookie = "Authorization=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+            window.location.href = '/auth/login'
+        }
+        return Promise.reject(error)
+    }
+)
+
+export default api
--- src/pages/auth/auth-components/RequireAuth.tsx
+++ src/pages/auth/auth-components/RequireAuth.tsx
@@ -1,10 +1,37 @@
 import { AuthContext } from "@/context/AuthProvider";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router";
+import api from "../../../api/posts";
+import { Loader2 } from "lucide-react";
 
 export const RequireAuth = () => {
-    const { auth } = useContext(AuthContext);
+    const { setAuth } = useContext(AuthContext)
+    const [response, setResponse] = useState<any>(null)
+    const [loading, setLoading] = useState(true)
+    const [unauthorized, setUnauthorized] = useState(false)
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await api.get('/validate')
+                setResponse(response)
+                setAuth({ userId: response.data.message.userId, firstName: response.data.message.firstName, email: response.data.message.email })
+                console.log(response.data.message)
+                setLoading(false)
+            } catch (error) {
+                console.error("Error fetching data:", error)
+                console.log(error)
+                setResponse(error)
+                setUnauthorized(true)
+                setLoading(false)
+            }
+        }
+        fetchData()
+    }, [])
+    if (loading) return <div className="w-screen h-screen flex items-center justify-center">
+        <Loader2 className="w-10 h-10 animate-spin" />
+    </div>
+    if (unauthorized) return <Navigate to="/auth/login" replace />
     return (
-        auth?.userId ? <Outlet /> : <Navigate to="/auth/login" replace />
+        response?.status === 401 ? <Navigate to="/auth/login" replace /> : <Outlet />
     )
 };
